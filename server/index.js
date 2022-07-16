94dd5fe7c623530248b6eff09724d07cf76a99b7
jmbp1999 <pamudithaj98@gmail.com>
2022-07-16 14:45:32 +0530

    auth functionalities and component implementation

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from './routes/user.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRouter);
 
 const CONNECTION_URL = 'mongodb://localhost:27017/foodie';
 const PORT = process.env.PORT || 5000;
