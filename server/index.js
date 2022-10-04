ce84f757fe314bfb1c1fb16fcba53642b18ee968
jmbp1999 <pamudithaj98@gmail.com>
2022-10-04 11:19:03 +0530

    added deployment details

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
-const CONNECTION_URL = 'mongodb://localhost:27017/foodie';
+const CONNECTION_URL = 'mongodb+srv://admin-jmbp:paty1234@cluster0.89zhh.mongodb.net/travelApp?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
